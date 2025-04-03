b04b338ec9cbbd0350b48ec83cf1ebfe8847c2bb
Ziyad-Bin-Sulfi <Ziyad.Bin.Sulfi@ibm.com>
2025-04-04 00:11:07 +0530

    Updated minor fix in the code

--- Challenge-18/CoinChange.java
+++ Challenge-18/CoinChange.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
@@ -9,10 +12,15 @@ public class CoinChange {
                 }
             }
         }
-        System.out.println(Arrays.toString(dp));
         if (dp[amount] != amount + 1) {
             return dp[amount];
         }
         return -1;
     }    
+    public static void main(String[] args) {
+        CoinChange s = new CoinChange();
+        int[] intArr = {1,3,4,5};
+        int result = s.coinChange(intArr, 7);
+        System.out.println(result);
+    }
 }
